ce901ccd31273c58794874708a319094ee8da4c5
madeibao <2901429479@qq.com>
2026-09-17 18:06:29 +0800

    fix some demo

--- PartC/py字母的移位词语分组.py
+++ PartC/py字母的移位词语分组.py
@@ -1,28 +1,20 @@
-
-
-
 # 字母的移位词语分组单词。
+from typing import List
 
 
 class Solution(object):
-	def group(self,strs):
+    def group(self, strs:List[str]) -> List[List[str]]:
+        dict2 = {}
+        for i in strs:
+            temp = "".join(sorted(list(i)))
+            if temp in dict2:
+                dict2[temp].append(i)
+            else:
+                dict2[temp] = [i]
+        return list(dict2.values())
 
-
-		dict2 = {}
-
-		for i in strs:
-			temp = "".join(sorted(list(i)))
-
-			if temp in dict2:
-				dict2[temp].append(i)
-			else:
-				dict2[temp] = [i]
-
-		return list(dict2.values())
 
 if __name__ == "__main__":
-	s = Solution()
-	list2 = ["eat", "tea", "tan", "ate", "nat", "bat"]
-	print(s.group(list2))
-
-	
+    s = Solution()
+    list2 = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    print(s.group(list2))
