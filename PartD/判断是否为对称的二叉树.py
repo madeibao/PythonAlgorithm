45d2a35b8fc590d8fc4ac0a88ce048ea249eff01
madeibao <2901429479@qq.com>
2026-09-17 17:33:05 +0800

    fix some demo

--- PartD/判断是否为对称的二叉树.py
+++ PartD/判断是否为对称的二叉树.py
@@ -1,5 +1,3 @@
-
-
 # 判断是否为对称的二叉树。
 
 
@@ -10,21 +8,18 @@ class TreeNode(object):
         self.right = None
 
 
-class Solution(object):
-    def valid(self, root):
+def valid(root):
+    def helper(left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return left.val == right.val and helper(left.left, right.right) and helper(left.right, right.left)
 
-        def helper(left, right):
-            if not left and not right:
-                return True
-            if not left or not right:
-                return False
-            return left.val == right.val and helper(left.left, right.right) and helper(left.right, right.left)
-
-        return helper(root, root)
+    return helper(root, root)
 
 
 if __name__ == '__main__':
-    s = Solution()
     head = TreeNode(1)
     h2 = TreeNode(2)
     h3 = TreeNode(2)
@@ -32,5 +27,4 @@ if __name__ == '__main__':
     head.left = h2
     head.right = h3
 
-    print(s.valid(head))
-
+    print(valid(head))
