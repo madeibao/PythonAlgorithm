ba8fd395b397aea7457928d2a1ed02fdd87f38ec
madeibao <2901429479@qq.com>
2026-09-17 11:37:22 +0800

    fix some demo

--- PartC/Py一周中的第几天.py
+++ PartC/Py一周中的第几天.py
@@ -1,17 +1,12 @@
+from datetime import datetime
 
+def getWeekDay(day, month, year):
+    d = datetime(year, month, day)
+    week_map = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    return week_map[d.weekday()]
 
-class Solution():
-	def getWeekDay(self, day, month, year):
-		from datetime import datetime
-		temp = ["Friday", "Saturday", "Monday", "Tuesday", "Wednesday","Thursday"]
-
-		day1 = datetime.strptime('1971-01-01','%Y-%m-%d')
-		day2 = datetime.strptime(str(year)+'-'+str(month)+'-'+str(day),'%Y-%m-%d')
-
-		return temp[(day2-day1).days%7]
-
+class Solution:
+    pass
 
 if __name__ == '__main__':
-
-	s = Solution()
-	print(s.getWeekDay(26, 10, 2019))
+    print(getWeekDay(17, 9, 2026))  # Thursday
