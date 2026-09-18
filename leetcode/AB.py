49d07c0ced4b6a5ec8bbde88df0d40a77a3dded1
madeibao <2901429479@qq.com>
2026-09-18 18:21:54 +0800

    fix some demo

--- leetcode/AB.py
+++ leetcode/AB.py
@@ -3,19 +3,21 @@
 # @Time: 2026/9/18/星期五 10:20
 # @File: AB.py
 
+from typing import List
+
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         # for i in range(len(nums)):
         #     for j in range(i + 1, len(nums)):
         #         if nums[i] + nums[j] == target:
         #             return [i, j]
         # return []
 
-        dict = {}
+        dicta = {}
         for i in range(len(nums)):
-            if target - nums[i] in dict:
-                return [dict[target - nums[i]], i]
-            dict[nums[i]] = i
+            if target - nums[i] in dicta:
+                return [dicta[target - nums[i]], i]
+            dicta[nums[i]] = i
         return []
 
 if __name__ == '__main__':
