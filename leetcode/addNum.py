9f7709c070dceafd11d3775bd4e25662e3481804
madeibao <2901429479@qq.com>
2026-09-20 17:14:21 +0800

    fix some explanation

--- leetcode/addNum.py
+++ leetcode/addNum.py
@@ -2,10 +2,11 @@
 # @Author: Mayuan
 # @Time: 2026/9/20/星期日 11:09
 # @File: addNum
+from typing import List
 
 
 class Solution:
-    def plusOne(self, digits: list[int]) -> list[int]:
+    def plusOne(self, digits: List[int]) -> List[int]:
         num = int("".join(map(str, digits)))
         num += 1
         return list(map(int, str(num)))
