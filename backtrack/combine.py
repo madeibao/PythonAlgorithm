3535c8d8639bfde6d9503d2b5bf6020449c7d3fb
madeibao <2901429479@qq.com>
2026-09-23 21:43:26 +0800

    add combine demo

---
+++ backtrack/combine.py
@@ -0,0 +1,29 @@
+
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        path = []
+
+        def backtrack(start):
+            if len(path) == k:
+                res.append(path.copy())
+                return
+            for i in range(start, n + 1):
+                path.append(i)
+                backtrack(i + 1)
+                path.pop()
+
+        backtrack(1)
+        return res
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    n = 4
+    k = 2
+    print(solution.combine(n, k))
+
+    
