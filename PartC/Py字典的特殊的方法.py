0686c5321e49f3b97334f7dfc566151694bb7f32
madeibao <2901429479@qq.com>
2026-09-26 21:44:22 +0800

    add some demo

--- PartC/Py字典的特殊的方法.py
+++ PartC/Py字典的特殊的方法.py
@@ -1,4 +1,5 @@
 
+from collections import Counter
 # 字典的内容，会根据列表的出现的顺序。
 
 list2 = ['B','A','D','C','A']
@@ -9,3 +10,8 @@ for i in list2:
 
 print(dict2)
 
+print('-------------------')
+list3 = ['B','A','D','C','A']
+# 直接使用Counter来统计列表中元素的出现次数
+dict3 = Counter(list3) 
+print(dict3)
